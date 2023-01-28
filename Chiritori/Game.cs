d021c64de18eb1be31d3db631d4a28236bc05a2b
XUZHENYU <G022G1271@g.neec.ac.jp>
2023-01-28 09:38:09 +0800

    チリトリーの作成

--- Chiritori/Game.cs
+++ Chiritori/Game.cs
@@ -5,21 +5,27 @@ namespace Chiritori
 {
     public class Game
     {
+        Chiritori chiritori;
         public void Init()
         {
             Image.Load();
             MyRandom.Init();
             Input.Init();
+            chiritori = new Chiritori();
         }
 
         public void Update()
         {
             Input.Update();
+
+            chiritori.Update();
         }
 
         public void Draw()
         {
             DX.DrawGraph(0, 0, Image.woodFloor);
+
+            chiritori.Draw();
         }
     }
 }
